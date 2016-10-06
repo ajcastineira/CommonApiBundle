e8c101c0bd27fa1244066cdc6afe79ec685687c5
Artem Genvald <genvaldartem@gmail.com>
2016-10-06 20:28:26 +0300

    Add JsonDecoder

---
+++ EventListener/JsonDecoderListener.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the FreshCommonApiBundle
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\CommonApiBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * JsonDecoderListener.
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+class JsonDecoderListener
+{
+    /**
+     * @var array $jsonContentTypes JSON content types
+     */
+    private $jsonContentTypes = [
+        'application/json',
+    ];
+
+    /**
+     * On kernel request.
+     *
+     * @param GetResponseEvent $event Event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if ($request->headers->has('Content-Type')) {
+            $contentType = $request->headers->get('Content-Type');
+
+            if (in_array($contentType, $this->jsonContentTypes)) {
+                $data = json_decode($request->getContent(), true);
+
+                if (is_array($data)) {
+                    $request->request = new ParameterBag($data);
+                }
+            }
+        }
+    }
+}
